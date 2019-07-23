a6bf6690dadbf9e537fc104e6e33e81bb9e17ab0
Devraj Sahu <sahudevraj4@gmail.com>
2019-07-23 14:09:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import * as serviceWorker from './serviceWorker';
 // import App from './Chat/App';
 //import MyComp from './class1/mycomp';
 // import Class8 from "./class8/class8";
-// import Router from "./Proj/Router";
-import Try from './Proj/try';
+import Router from "./Proj/Router";
+// import Try from './Proj/try';
 
 
-ReactDOM.render(<Try />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
